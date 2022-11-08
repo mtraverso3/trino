7f2694ddb43fea3d9f7d12b0b517515374810ab5
Marcos Traverso <marcostraverso2003@gmail.com>
2022-11-08 16:28:06 -0500

    Migrate TestTime to QueryAssertions

--- core/trino-main/src/test/java/io/trino/type/TestTime.java
+++ core/trino-main/src/test/java/io/trino/type/TestTime.java
@@ -13,125 +13,126 @@
  */
 package io.trino.type;
 
-import io.trino.operator.scalar.AbstractTestFunctions;
-import io.trino.spi.type.TimeZoneKey;
-import io.trino.testing.TestingSession;
-import org.joda.time.DateTimeZone;
-import org.testng.annotations.Test;
+import io.trino.sql.query.QueryAssertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import static io.trino.spi.function.OperatorType.EQUAL;
 import static io.trino.spi.function.OperatorType.INDETERMINATE;
-import static io.trino.spi.type.BooleanType.BOOLEAN;
-import static io.trino.spi.type.VarcharType.VARCHAR;
-import static io.trino.testing.TestingSession.testSessionBuilder;
-import static io.trino.type.IntervalDayTimeType.INTERVAL_DAY_TIME;
-import static io.trino.util.DateTimeZoneIndex.getDateTimeZone;
+import static io.trino.spi.function.OperatorType.LESS_THAN;
+import static io.trino.spi.function.OperatorType.LESS_THAN_OR_EQUAL;
+import static io.trino.spi.function.OperatorType.SUBTRACT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+@TestInstance(PER_CLASS)
 public class TestTime
-        extends AbstractTestFunctions
 {
-    protected static final TimeZoneKey TIME_ZONE_KEY = TestingSession.DEFAULT_TIME_ZONE_KEY;
-    protected static final DateTimeZone DATE_TIME_ZONE = getDateTimeZone(TIME_ZONE_KEY);
+    private QueryAssertions assertions;
 
-    public TestTime()
+    @BeforeAll
+    public void init()
     {
-        super(testSessionBuilder()
-                .setTimeZoneKey(TIME_ZONE_KEY)
-                .build());
+        assertions = new QueryAssertions();
+    }
+
+    @AfterAll
+    public void teardown()
+    {
+        assertions.close();
+        assertions = null;
     }
 
     @Test
     public void testSubtract()
     {
-        functionAssertions.assertFunctionString("TIME '14:15:16.432' - TIME '03:04:05.321'", INTERVAL_DAY_TIME, "0 11:11:11.111");
+        assertThat(assertions.operator(SUBTRACT, "TIME '14:15:16.432'", "TIME '03:04:05.321'"))
+                .isEqualTo(new SqlIntervalDayTime(0, 11, 11, 11, 111));
 
-        functionAssertions.assertFunctionString("TIME '03:04:05.321' - TIME '14:15:16.432'", INTERVAL_DAY_TIME, "-0 11:11:11.111");
+        assertThat(assertions.operator(SUBTRACT, "TIME '03:04:05.321'", "TIME '14:15:16.432'"))
+                .isEqualTo(new SqlIntervalDayTime(0, -11, -11, -11, -111));
     }
 
     @Test
     public void testEqual()
     {
-        assertFunction("TIME '03:04:05.321' = TIME '03:04:05.321'", BOOLEAN, true);
-
-        assertFunction("TIME '03:04:05.321' = TIME '03:04:05.333'", BOOLEAN, false);
+        assertThat(assertions.operator(EQUAL, "TIME '03:04:05.321'", "TIME '03:04:05.321'")).isEqualTo(true);
+        assertThat(assertions.operator(EQUAL, "TIME '03:04:05.321'", "TIME '03:04:05.333'")).isEqualTo(false);
     }
 
     @Test
     public void testNotEqual()
     {
-        assertFunction("TIME '03:04:05.321' <> TIME '03:04:05.333'", BOOLEAN, true);
-
-        assertFunction("TIME '03:04:05.321' <> TIME '03:04:05.321'", BOOLEAN, false);
+        assertThat(assertions.expression("TIME '03:04:05.321' <> TIME '03:04:05.333'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' <> TIME '03:04:05.321'")).isEqualTo(false);
     }
 
     @Test
     public void testLessThan()
     {
-        assertFunction("TIME '03:04:05.321' < TIME '03:04:05.333'", BOOLEAN, true);
-
-        assertFunction("TIME '03:04:05.321' < TIME '03:04:05.321'", BOOLEAN, false);
-        assertFunction("TIME '03:04:05.321' < TIME '03:04:05'", BOOLEAN, false);
+        assertThat(assertions.operator(LESS_THAN, "TIME '03:04:05.321'", "TIME '03:04:05.333'")).isEqualTo(true);
+        assertThat(assertions.operator(LESS_THAN, "TIME '03:04:05.321'", "TIME '03:04:05.321'")).isEqualTo(false);
+        assertThat(assertions.operator(LESS_THAN, "TIME '03:04:05.321'", "TIME '03:04:05'")).isEqualTo(false);
     }
 
     @Test
     public void testLessThanOrEqual()
     {
-        assertFunction("TIME '03:04:05.321' <= TIME '03:04:05.333'", BOOLEAN, true);
-        assertFunction("TIME '03:04:05.321' <= TIME '03:04:05.321'", BOOLEAN, true);
-
-        assertFunction("TIME '03:04:05.321' <= TIME '03:04:05'", BOOLEAN, false);
+        assertThat(assertions.operator(LESS_THAN_OR_EQUAL, "TIME '03:04:05.321'", "TIME '03:04:05.333'")).isEqualTo(true);
+        assertThat(assertions.operator(LESS_THAN_OR_EQUAL, "TIME '03:04:05.321'", "TIME '03:04:05.321'")).isEqualTo(true);
+        assertThat(assertions.operator(LESS_THAN_OR_EQUAL, "TIME '03:04:05.321'", "TIME '03:04:05'")).isEqualTo(false);
     }
 
     @Test
     public void testGreaterThan()
     {
-        assertFunction("TIME '03:04:05.321' > TIME '03:04:05.111'", BOOLEAN, true);
-
-        assertFunction("TIME '03:04:05.321' > TIME '03:04:05.321'", BOOLEAN, false);
-        assertFunction("TIME '03:04:05.321' > TIME '03:04:05.333'", BOOLEAN, false);
+        assertThat(assertions.expression("TIME '03:04:05.321' > TIME '03:04:05.111'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' > TIME '03:04:05.321'")).isEqualTo(false);
+        assertThat(assertions.expression("TIME '03:04:05.321' > TIME '03:04:05.333'")).isEqualTo(false);
     }
 
     @Test
     public void testGreaterThanOrEqual()
     {
-        assertFunction("TIME '03:04:05.321' >= TIME '03:04:05.111'", BOOLEAN, true);
-        assertFunction("TIME '03:04:05.321' >= TIME '03:04:05.321'", BOOLEAN, true);
-
-        assertFunction("TIME '03:04:05.321' >= TIME '03:04:05.333'", BOOLEAN, false);
+        assertThat(assertions.expression("TIME '03:04:05.321' >= TIME '03:04:05.111'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' >= TIME '03:04:05.321'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' >= TIME '03:04:05.333'")).isEqualTo(false);
     }
 
     @Test
     public void testBetween()
     {
-        assertFunction("TIME '03:04:05.321' between TIME '03:04:05.111' and TIME '03:04:05.333'", BOOLEAN, true);
-        assertFunction("TIME '03:04:05.321' between TIME '03:04:05.321' and TIME '03:04:05.333'", BOOLEAN, true);
-        assertFunction("TIME '03:04:05.321' between TIME '03:04:05.111' and TIME '03:04:05.321'", BOOLEAN, true);
-        assertFunction("TIME '03:04:05.321' between TIME '03:04:05.321' and TIME '03:04:05.321'", BOOLEAN, true);
-
-        assertFunction("TIME '03:04:05.321' between TIME '03:04:05.322' and TIME '03:04:05.333'", BOOLEAN, false);
-        assertFunction("TIME '03:04:05.321' between TIME '03:04:05.311' and TIME '03:04:05.312'", BOOLEAN, false);
-        assertFunction("TIME '03:04:05.321' between TIME '03:04:05.333' and TIME '03:04:05.111'", BOOLEAN, false);
+        assertThat(assertions.expression("TIME '03:04:05.321' between TIME '03:04:05.111' and TIME '03:04:05.333'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' between TIME '03:04:05.321' and TIME '03:04:05.333'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' between TIME '03:04:05.111' and TIME '03:04:05.321'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' between TIME '03:04:05.321' and TIME '03:04:05.321'")).isEqualTo(true);
+        assertThat(assertions.expression("TIME '03:04:05.321' between TIME '03:04:05.322' and TIME '03:04:05.333'")).isEqualTo(false);
+        assertThat(assertions.expression("TIME '03:04:05.321' between TIME '03:04:05.311' and TIME '03:04:05.312'")).isEqualTo(false);
+        assertThat(assertions.expression("TIME '03:04:05.321' between TIME '03:04:05.333' and TIME '03:04:05.111'")).isEqualTo(false);
     }
 
     @Test
     public void testCastToVarchar()
     {
-        assertFunction("cast(TIME '03:04:05.321' as varchar)", VARCHAR, "03:04:05.321");
-        assertFunction("cast(TIME '03:04:05' as varchar)", VARCHAR, "03:04:05");
-        assertFunction("cast(TIME '03:04' as varchar)", VARCHAR, "03:04:00");
+        assertThat(assertions.expression("cast(TIME '03:04:05.321' as varchar)")).isEqualTo("03:04:05.321");
+        assertThat(assertions.expression("cast(TIME '03:04:05' as varchar)")).isEqualTo("03:04:05");
+        assertThat(assertions.expression("cast(TIME '03:04' as varchar)")).isEqualTo("03:04:00");
     }
 
     @Test
     public void testCastFromVarchar()
     {
-        assertFunction("cast('03:04:05.321' as time) = TIME '03:04:05.321'", BOOLEAN, true);
-        assertFunction("cast('03:04:05' as time) = TIME '03:04:05.000'", BOOLEAN, true);
-        assertFunction("cast('03:04' as time) = TIME '03:04:00.000'", BOOLEAN, true);
+        assertThat(assertions.operator(EQUAL, "cast('03:04:05.321' as time)", "TIME '03:04:05.321'")).isEqualTo(true);
+        assertThat(assertions.operator(EQUAL, "cast('03:04:05' as time)", "TIME '03:04:05.000'")).isEqualTo(true);
+        assertThat(assertions.operator(EQUAL, "cast('03:04' as time)", "TIME '03:04:00.000'")).isEqualTo(true);
     }
 
     @Test
     public void testIndeterminate()
     {
-        assertOperator(INDETERMINATE, "cast(null as TIME)", BOOLEAN, true);
-        assertOperator(INDETERMINATE, "TIME '00:00:00'", BOOLEAN, false);
+        assertThat(assertions.operator(INDETERMINATE, "cast(null as TIME)")).isEqualTo(true);
+        assertThat(assertions.operator(INDETERMINATE, "TIME '00:00:00'")).isEqualTo(false);
     }
 }
